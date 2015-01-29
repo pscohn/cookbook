3c181c078a7378373ac9468add6cda1c8601fcf2
Paul Cohn <pscohn@gmail.com>
2015-01-29 16:03:56 -0500

    Initial lonely integer algo

--- algorithms/lonely_integer.py
+++ algorithms/lonely_integer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
 def lonely_integer(array):
+    count = defaultdict(int)
+    for i in array:
+        count[i] += 1
+    for k, v in count.items():
+        if v == 1:
+            return k
 
 def main():
     size = input()
     items = input()
     array = items.split()
-    lonely_integer = lonely_integer(array)
-
+    result = lonely_integer(array)
+    print(result)
 
 
 if __name__ == '__main__':
