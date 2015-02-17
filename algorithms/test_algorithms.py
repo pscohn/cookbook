b2263d703e4d5d8609751a0c5cacf659ec032298
Paul Cohn <pscohn@gmail.com>
2015-02-17 16:52:14 -0500

    Testcase for binary search

--- algorithms/test_algorithms.py
+++ algorithms/test_algorithms.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 from lonely_integer import lonely_integer
+from algorithms import *
 
 class TestAlgorithms(unittest.TestCase):
     def test_lonely_integer(self):
@@ -8,8 +9,12 @@ class TestAlgorithms(unittest.TestCase):
         result = 3
         self.assertEqual(result, lonely_integer(array))
 
-
-
+    def test_binary_search(self):
+        array = [1, 3, 4, 8, 23, 49, 482, 849]
+        self.assertEqual(2, binary_search(array, 4, 0, len(array)))
+        self.assertEqual(5, binary_search(array, 49, 0, len(array)))
+        self.assertEqual(7, binary_search(array, 849, 0, len(array)))
+        self.assertEqual(None, binary_search(array, 50, 0, len(array)))
 
 if __name__ == '__main__':
     unittest.main()
